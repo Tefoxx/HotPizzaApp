c38394ac208a2604397a95af1d77301241489224
OS4mainHUB <os4.official@gmail.com>
2021-10-28 01:01:38 +0700

    Add EditText 1.1

--- app/src/main/java/com/example/hotpizzaapp/MenuFragment.kt
+++ app/src/main/java/com/example/hotpizzaapp/MenuFragment.kt
@@ -3,20 +3,41 @@ package com.example.hotpizzaapp
 import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import android.view.*
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.SearchView
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.hotpizzaapp.adapters.PizzaAdapter
-import com.example.hotpizzaapp.data.PizzaEntity
+
 import com.example.hotpizzaapp.data.remote.PizzaListItem
 import com.example.hotpizzaapp.databinding.FragmentMenuBinding
 import com.example.hotpizzaapp.models.MenuFragmentViewModel
 import okhttp3.internal.notify
+
+import android.widget.TextView
+
+import android.widget.TextView.OnEditorActionListener
+import android.app.Activity
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+
+import androidx.core.content.ContextCompat.getSystemService
+
+
+
+
+
+
+
+
+
 
 
 class MenuFragment : Fragment() {
@@ -53,29 +74,57 @@ class MenuFragment : Fragment() {
             })
         }
 
+//        binding.etSearchPizza.setOnFocusChangeListener { v, hasFocus ->
+//
+//        }
+
+
 
         binding.imgLoopa.setOnClickListener {
             with(binding){
                 tvMenuFragment.visibility = View.INVISIBLE
                 etSearchPizza.visibility = View.VISIBLE
+                etSearchPizza.text.clear()
                 imgLoopa.visibility = View.INVISIBLE
+                etSearchPizza.requestFocus()
+                val inputMethodManager = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.showSoftInput(etSearchPizza, InputMethodManager.SHOW_IMPLICIT)
             }
         }
 
+        binding.etSearchPizza.addTextChangedListener {
+            with(binding){
+
+                val resList = mutableListOf<PizzaListItem>()
+                val edit = it
+                if(it != null) {
+                   viewModel?.pizzaList?.value?.forEach {
+                        if(it.name.lowercase().contains(edit.toString()))
+                            resList.add(it)
+                    }
+                    viewModel?.pizzaListOpen?.value = resList
+                }
+            }
+        }
+
+
+        binding.etSearchPizza.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                with(binding) {
+                    tvMenuFragment.visibility = View.VISIBLE
+                    imgLoopa.visibility = View.VISIBLE
+                    etSearchPizza.visibility = View.GONE
+
+                }
+                val imm =
+                    activity?.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager?
+                imm?.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0)
+            }
+            true
+        }
+
         //Был спидран по SearchView. Было бы хорошо узнать от опытного разраба, как всё таки сделать правильно всё это, а не эти костыли :(
-//        binding.imgLoopa.setOnClickListener {
-//
-//            with(binding){
-//                tvMenuFragment.visibility = View.INVISIBLE
-//                searchView.visibility = View.VISIBLE
-//                imgLoopa.visibility = View.INVISIBLE
-//                searchView.requestFocus()
-//                val inputMethodManager = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-//                inputMethodManager.showSoftInput(searchView, InputMethodManager.SHOW_IMPLICIT)
-//                searchView.setQuery("", false)
-//            }
-//
-//        }
+
 //
 //        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
 //            override fun onQueryTextSubmit(query: String?): Boolean {
